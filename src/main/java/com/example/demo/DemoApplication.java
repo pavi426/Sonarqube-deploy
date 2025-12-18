6d64324b6350e26e77add98de136ac6693e7d342
pavi426 <pavi426@gmail.com>
2025-12-18 20:10:01 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,20 @@
-@RestController
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "CI/CD Pipeline Success";
+        return "Hello from Jenkins CI/CD Pipeline!";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
-
